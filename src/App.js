2057bae4138553852e70c36c30c54a1c7eeeac1f
prasad66 <ndpk.prasad@gmail.com>
2022-03-28 14:40:10 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import RoomIcon from '@mui/icons-material/Room';
